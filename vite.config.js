6287542534e9130584b825e5acd03c5937250a93
eren <69261887+ErenKarakas1@users.noreply.github.com>
2024-01-11 07:14:53 +0300

    improve folder structure

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vuetify from "vite-plugin-vuetify";
 
-// https://vitejs.dev/config/
 export default defineConfig(async () => ({
   plugins: [
     vue(),
@@ -10,7 +9,6 @@ export default defineConfig(async () => ({
   ],
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
-  //
   // 1. prevent vite from obscuring rust errors
   clearScreen: false,
   // 2. tauri expects a fixed port, fail if that port is not available
@@ -19,7 +17,7 @@ export default defineConfig(async () => ({
     strictPort: true,
     watch: {
       // 3. tell vite to ignore watching `src-tauri`
-      ignored: ["**/src-tauri/**"],
+      ignored: ["**/app/**"],
     },
   },
 }));
